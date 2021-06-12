ab0371d1af2fd76fb6863b966afbe119a67fc6fb
Kalen Gladu-Lauridsen <62851055+KalenGladu-Lauridsen@users.noreply.github.com>
2021-06-12 10:51:23 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Lab3Cs.Models;
+
+namespace Lab3Cs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Count()
+        {
+            HttpContext.Session.SetString("bottleAmount", Request.Form["bottleAmount"]);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person model)
+        {
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3Cs.Models
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(100)]
+        [BindProperty(Name = "firstName")]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(100)]
+        [BindProperty(Name = "lastName")]
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [BindProperty(Name = "age")]
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        [BindProperty(Name = "email")]
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [BindProperty(Name = "date")]
+        public string BirthDay
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(500)]
+        [BindProperty(Name = "description")]
+        public string PersonDescription
+        {
+            get;
+            set;
+        }
+    }
+}
